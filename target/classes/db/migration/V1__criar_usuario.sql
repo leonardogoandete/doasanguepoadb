6ba306afb582fae4f7ce0522627f3959189762ca
leonardo_goandete <leonardo_goandete@sicredi.com.br>
2022-08-31 12:37:42 -0300

    First commit

--- target/classes/db/migration/V1__criar_usuario.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE usuario (
-	id int8 NOT NULL,
-	nome varchar(255) NOT NULL,
-	idade integer NOT NULL,
-	CONSTRAINT usuario_pkey PRIMARY KEY (id)
-);
-
-insert into usuario(id, nome, idade) values (1,  'Nataniel', 29);
